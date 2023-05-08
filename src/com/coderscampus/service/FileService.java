5a5286225bd5a82ccd8d12332a7e369d0f0f8204
tp02ga <trevor@craftycodr.com>
2023-05-08 13:56:00 -0400

    23 - Using ArrayLists

--- src/com/coderscampus/service/FileService.java
+++ src/com/coderscampus/service/FileService.java
@@ -3,6 +3,7 @@ package com.coderscampus.service;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class FileService {
 	
@@ -22,15 +23,20 @@ public class FileService {
 	public void printFileContentsToConsole (Path path) {
 		try {
 			
-			System.out.println( Files.readAllLines(path) );
-			throw new IllegalArgumentException(); // Note: this is not real-world code, you wouldn't just throw
-			                                      // an exception like this for no reason.
+			// Step 1: Read data in
+			ArrayList<String> data = (ArrayList<String>) Files.readAllLines(path);
+			
+			// Step 2: do something useful with the data
+			
+			// Step 3: Output the data
+			for ( int i = 0; i < data.size(); i++ ) {
+				System.out.println(data.get(i));
+			}
+			
+			// Step 4: Profit
 		} catch (IOException e) {
 			System.out.println("Oops, there was a problem reading the file");
 			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			System.out.println("Hey there, we caught the IllegalArgumentException, aren't we silly");
-			e.printStackTrace();
-		}
+		} 
 	}
 }
